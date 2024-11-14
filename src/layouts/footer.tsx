56cc4879b6a9d4748fcf180f8c326436a646ad57
4LastBreath <04lastbreath@gmail.com>
2024-11-14 21:09:43 +0100

    new portfolio url

--- src/layouts/footer.tsx
+++ src/layouts/footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
 <footer className='bg-neutral-300 dark:bg-card w-full h-[34px] flex items-center justify-center'>
   <p className='text-sm text-neutral-700 dark:text-neutral-400'>
     Made with love by 
-    <a href='https://4lastbreath.github.io/portfolio/' target="_blank" className='hover:text-primary'> Brian Fritz </a>
+    <a href='https://bf-dev.space/' target="_blank" className='hover:text-primary'> Brian Fritz </a>
     , 2024©
   </p>
 </footer>
