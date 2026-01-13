b22df574604688e420db057c09cde7f90b23b2cd
4LastBreath <04lastbreath@gmail.com>
2026-01-13 14:47:55 +0100

    audit fix

--- src/layouts/footer.tsx
+++ src/layouts/footer.tsx
@@ -1,11 +1,14 @@
 
 const Footer = () => {
+
+  const currentYear = new Date().getFullYear()
+
   return (
 <footer className='bg-neutral-300 dark:bg-card w-full h-[34px] flex items-center justify-center'>
   <p className='text-sm text-neutral-700 dark:text-neutral-400'>
     Made with love by 
     <a href='https://bf-dev.space/' target="_blank" className='hover:text-primary'> Brian Fritz </a>
-    , 2024©
+    , {currentYear}&copy;
   </p>
 </footer>
   );
